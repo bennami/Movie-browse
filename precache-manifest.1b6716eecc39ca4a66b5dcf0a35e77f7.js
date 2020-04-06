194023cf99528d26a9903da380ec6c3fc74567c0
imane <benbenimane@hotmail.com>
2020-04-06 21:37:14 +0200

    Updates

---
+++ precache-manifest.1b6716eecc39ca4a66b5dcf0a35e77f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65abfda8c6ca97826a8e015c86064a3b",
+    "url": "/Movie-browse/index.html"
+  },
+  {
+    "revision": "eb8710ecade0213d026e",
+    "url": "/Movie-browse/static/css/main.e479ea35.chunk.css"
+  },
+  {
+    "revision": "003cf805ffb2aaa72507",
+    "url": "/Movie-browse/static/js/2.8ce205a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-browse/static/js/2.8ce205a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8710ecade0213d026e",
+    "url": "/Movie-browse/static/js/main.d9a9818e.chunk.js"
+  },
+  {
+    "revision": "a4c71dbd2c8cba9ef66b",
+    "url": "/Movie-browse/static/js/runtime-main.b86ed7a1.js"
+  }
+]);
