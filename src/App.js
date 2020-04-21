ba307e3ec65d1553f4d994c80c64047777300660
imane <benbenimane@hotmail.com>
2020-04-21 20:04:19 +0200

    removed bunch of unnecessary stuff i didnt need on app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-import React, {useState, useEffect} from 'react';
-import List from "./Components/List";
+import React, {useState} from 'react';
 import SearchBar from "./Components/searchbar";
 import Nav from "./Components/nav";
 import Routes from "./routes";
-import {useParams,useHistory} from "react-router-dom";
-
+import {useHistory} from "react-router-dom";
 import './App.css';
-import Search from "./pages/search";
+
 
 function App() {
   //const apiKey =  process.env.REACT_APP_API;
   const [search, setSearch]  = useState('');
   const  [movieList,  setMovieList]  = useState([]);
-  const [isSubmitSearch, setSubmit] =useState(false);
-  const [popularMovies, setPopular] = useState([]);
-
-    const history=useHistory();
-
-    //on load, load List with popular movies
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch (`https://api.themoviedb.org/3/movie/popular?api_key=67b347978ffe14fc5d6f8a664a1829f2&language=en-US&page=1`);
-            const popular = await response.json();
-            setPopular(popular.results);
-        }
-        fetchData();
-    }, []);
 
 
-
-console.log(history);
-
+  const history=useHistory();
   //when user searches for a movie, fetch data and set movieList
   const searchItem = async (e) => {
     e.preventDefault();
-
-    //if search is empty
 
       if(search === ""){
           alert("please enter a movie title")
@@ -47,10 +27,8 @@ console.log(history);
 
           //set json object into moviesList array, every time there is a new search this will refresh itself
           setMovieList(data.results);
-         history.push(`/search/${search}`);
-          setSubmit(true);
+          history.push(`/search/${search}`);
       }
-
   };
 
   //set search while  typing
@@ -67,7 +45,6 @@ console.log(history);
                   handleSubmit={searchItem}
                   handleChange={handleChange}
               />
-             {/* <Search movieList={isSubmitSearch? movieList: popularMovies}/>*/}
               <Routes/>
           </div>
       )
