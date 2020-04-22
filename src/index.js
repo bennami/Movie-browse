e84578bbb7336a9e3b220c4fda81bb83c5d5e9aa
imane <benbenimane@hotmail.com>
2020-04-22 12:42:07 +0200

    hash

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import {BrowserRouter, HashRouter} from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
       <HashRouter>
-            <BrowserRouter>
+
             <App />
-            </BrowserRouter>
+          
       </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
