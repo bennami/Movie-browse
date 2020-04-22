b06925e2cc077cb9b20101aa7a909c2c6c953030
imane <benbenimane@hotmail.com>
2020-04-22 23:49:40 +0200

    solved problem with a combination of cors-everywhere dependency and hashing the router paths

--- src/pages/homeTrending.js
+++ src/pages/homeTrending.js
@@ -3,18 +3,14 @@ import List from "../Components/List";
 
 function Popular() {
 
-    const [popularMovies, setPopular] = useState([]);
+    const [TrendingTodayMovies, setTrending] = useState([]);
     const proxy = "https://cors-anywhere.herokuapp.com/";
     //on load, load List with popular movies
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch (`${proxy} https://api.themoviedb.org/3/trending/all/day?api_key=67b347978ffe14fc5d6f8a664a1829f2`);
-          /*  https://api.themoviedb.org/3/movie/popular?api_key=67b347978ffe14fc5d6f8a664a1829f2&language=en-US&page=1*/
-
-
-
-            const popular = await response.json();
-            setPopular(popular.results);
+            const response = await fetch (`${proxy}https://api.themoviedb.org/3/trending/all/day?api_key=67b347978ffe14fc5d6f8a664a1829f2`);
+            const trending = await response.json();
+            setTrending(trending.results);
         }
         fetchData();
     }, []);
@@ -26,7 +22,7 @@ function Popular() {
 
             </div>
         <h1>Trending right now</h1>
-        <List movieList={popularMovies}/>
+        <List movieList={TrendingTodayMovies}/>
         </div>
     )
 
