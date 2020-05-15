6d6c1746e4b9a6ede425323c55f2515b0d297099
imaneeeeeeee <49040881+bennami@users.noreply.github.com>
2020-05-15 23:11:47 +0200

    Update apiUtils.js

--- src/Api/apiUtils.js
+++ src/Api/apiUtils.js
@@ -1,17 +1,14 @@
 export async function handleResponse(response) {
     if (response.ok){
-        console.log('fetch success')GIT
+        console.log('fetch success')
         return response.json();}
     if (response.status === 400) {
-        // So, a server-side validation error occurred.
-        // Server side validation returns a string error message, so parse as text instead of json.
         const error = await response.text();
         console.log(error);
         throw new Error('404 error');
     }
     throw new Error("response failed");
 }
-
 
 export function handleError(error) {
 
