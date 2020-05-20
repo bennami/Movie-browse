59b2b636ac60289f422a0d011a7498eff37a4b90
imane <benbenimane@hotmail.com>
2020-05-20 11:33:33 +0200

    nothing

--- src/Api/movieApi.js
+++ src/Api/movieApi.js
@@ -1,6 +1,14 @@
 import { handleResponse, handleError } from "./apiUtils";
 import {API_KEY,BASE_URL,PROXY} from "../utils";
-import axios  from "axios"
+
+
+
+
+export function loadSearchResults(name,currentPage) {
+    return fetch(`${PROXY}${BASE_URL}search/movie/${API_KEY}&query=${name}&page=${currentPage}`)
+        .then(handleResponse)
+        .catch(handleError);
+}
 
 export function loadTrendingMovies() {
     return fetch(`${PROXY}${BASE_URL}/trending/all/day${API_KEY}`)
@@ -9,15 +17,8 @@ export function loadTrendingMovies() {
 
 }
 
-export function loadSearchResults(name,currentPage) {
-    return fetch(`${PROXY}${BASE_URL}search/movie/${API_KEY}&query=${name}&page=${currentPage}`)
-        .then(handleResponse)
-        .catch(handleError);
-}
-
-
 export function loadPopularMovies() {
-        return  fetch (`${PROXY}${BASE_URL}/movie/popular${API_KEY}&language=en-US&page=${"1"}`)
+        return  fetch (`${PROXY}${BASE_URL}/movie/popular${API_KEY}&language=en-US&page=1`)
             .then(handleResponse)
             .catch(handleError);
 }
